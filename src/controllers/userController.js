67b18cae1353f05fc52937c33a959c126f10b95d
athena272 <guilhermera272@gmail.com>
2023-04-09 01:38:17 -0300

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -27,4 +27,8 @@ function getUserById(req, res) {
     }
 }
 
-module.exports = { listUsers, getUserById }
+function createUser(req, res) {
+    
+}
+
+module.exports = { listUsers, getUserById, createUser }
